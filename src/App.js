fbfa45a0fb801389b9f9aed583a942ec471a0e15
katanabana <katanabana01@gmail.com>
2024-02-18 17:13:33 +0500

    add css for navbar

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ import Music from "./components/music/Music";
 import Settings from "./components/settings/Settings";
 import Profile from "./components/profile/Profile";
 
+
 function App() {
-  const menuNames = "home learn texts videos music settings profile".split(" ");
   return (
-    <div>
-      <Navbar menuNames={menuNames}></Navbar>
+    <>
+      <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/home" element={<Home></Home>}></Route>
-        <Route path="/learn" element={<Learn></Learn>}></Route>
-        <Route path="/texts" element={<Texts></Texts>}></Route>
-        <Route path="/videos" element={<Videos></Videos>}></Route>
-        <Route path="/music" element={<Music></Music>}></Route>
-        <Route path="/settings" element={<Settings></Settings>}></Route>
-        <Route path="/profile" element={<Profile></Profile>}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/learn" element={<Learn />} />
+        <Route path="/texts" element={<Texts />} />
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/music" element={<Music />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,46 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
-function Navbar({ menuNames }) {
+const menus = [
+  ["home", "learn"],
+  ["texts", "videos", "music"],
+  ["settings", "profile"],
+];
+
+const icons = {};
+for (const menu of menus.flat()) {
+  icons[menu] = (await import(`./icons/${menu}.png`)).default;
+}
+
+const capitalize = (string) => string[0].toUpperCase() + string.slice(1);
+
+function components(
+  collection,
+  { Component = "div", props = (element) => {}, content = (element) => <></> }
+) {
+  return Array.from(collection, (element) => (
+    <Component {...props(element)} key={String(element)}>
+      {content(element)}
+    </Component>
+  ));
+}
+
+function Navbar() {
   return (
-    <nav>
-      {menuNames.map((menuName) => (
-        <Link key={menuName} className={menuName} to={"/" + menuName}>
-          {menuName}
-        </Link>
-      ))}
+    <nav className="-bg-rect">
+      {components(menus, {
+        content: (group) =>
+          components(group, {
+            Component: NavLink,
+            props: (menu) => ({
+              to: "/" + menu,
+              className: ({ isActive }) => (isActive ? "" : "not-active"),
+            }),
+            content: (menu) => (
+              <img src={icons[menu]} alt={capitalize(menu)}></img>
+            ),
+          }),
+      })}
     </nav>
   );
 }
